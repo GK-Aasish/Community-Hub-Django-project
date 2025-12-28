c0638ea9cafae674143701cc737ca0e868bbffaa
Aasish Karki <gk.aasishkarki@gmail.com>
2025-12-28 15:36:26 +0545

    Initializing Django Templates

---
+++ c_hub/c_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views.comp_view import event_view,notice_view,meeting_view
+from .views.main_view import dashboard_view
+from .views.auth_view import login_view,signup_view
+
+urlpatterns = [
+    path('', dashboard_view, name='dashboard'),
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
+    path('event/', event_view, name='event'),
+    path('meeting/', meeting_view, name='meeting'),
+    path('notice/', notice_view, name='notice'),
+]
