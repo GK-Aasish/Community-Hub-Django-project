885e42421d958329fcc8dd5bc290703cc8554e6f
Aasish Karki <gk.aasishkarki@gmail.com>
2026-01-01 07:16:10 +0545

    Rearranging files

--- c_hub/c_app/views/comp_view.py
+++ c_hub/c_app/views/comp_view.py
@@ -1,10 +1,2 @@
 from django.shortcuts import render
 
-def notice_view (request):
-    return render(request, 'components/notice.html')
-
-def event_view (request):
-    return render(request, 'component/event.html')
-
-def meeting_view (request):
-    return render(request, 'component/meeting.html')
--- c_hub/c_app/views/main_view.py
+++ c_hub/c_app/views/main_view.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def dashboard_view (request):
     return render(request, 'main/index.html')
+
+def notice_view (request):
+    return render(request, 'main/notice.html')
+
+def event_view (request):
+    return render(request, 'main/event.html')
+
+def meeting_view (request):
+    return render(request, 'main/meeting.html')
